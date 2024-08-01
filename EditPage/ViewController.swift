1a6f36657b0aa84668b00d03f75c6b2a495037ea
Anton Ivchenko <kvadrostoplerman@gmail.com>
2024-08-01 07:03:32 +0300

    Initial Commit

---
+++ EditPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EditPage
+//
+//  Created by Anton Ivchenko on 01.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
